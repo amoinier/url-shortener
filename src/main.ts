5ea50dbe37c4f0ef2d68af8db4c526220f3efa63
Alex Moinier <me@amoinier.fr>
2026-03-05 21:03:17 +0100

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  console.log("Hello World");
+
+  res.json({ status: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
