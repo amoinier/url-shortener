51596b18e10f04d57962bb080a2fbb894c410818
Alex Moinier <me@amoinier.fr>
2026-03-06 17:25:41 +0100

    feat: improve security with cors+rateLimit+helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { configService } from './services/config'
 import { router } from './router'
 import { redisService } from './services/redis'
+import cors from 'cors'
+import helmet from 'helmet'
 
 async function startServer() {
   await redisService.init()
@@ -9,7 +11,11 @@ async function startServer() {
   const app = express()
   const port = configService.config.PORT
 
-  app.use(express.json())
+  app.use(express.json({ limit: '100mb' }))
+
+  app.use(helmet())
+
+  app.use(cors())
 
   app.use('/api', router())
 
@@ -32,4 +38,7 @@ async function startServer() {
   })
 }
 
-startServer()
+startServer().catch((error) => {
+  console.error('Failed to start server', { cause: error })
+  process.exit(1)
+})
