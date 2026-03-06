6e6537c8744ddfc9758e3890f62fc67c39e97295
Alex Moinier <me@amoinier.fr>
2026-03-06 21:21:39 +0100

    fix: improve security, set limiter on all endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,28 @@ import { router } from './router'
 import { redisService } from './services/redis'
 import cors from 'cors'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
-async function startServer () {
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 500,
+  standardHeaders: true,
+  legacyHeaders: false
+})
+
+async function startServer() {
   await redisService.init()
 
   const app = express()
   const port = configService.config.PORT
 
-  app.use(express.json({ limit: '100mb' }))
+  app.use(express.json({ limit: '10kb' }))
 
   app.use(helmet())
 
   app.use(cors())
 
-  app.use('/api', router())
+  app.use('/api', limiter, router())
 
   app.use(
     (
--- src/router.ts
+++ src/router.ts
@@ -3,19 +3,11 @@ import { shortUrlController } from './controllers/short-url/short-url'
 import { redirectUrlController } from './controllers/redirect-url/redirect-url'
 import { shortUrlAnalyticsController } from './controllers/short-url-analytics/short-url-analytics'
 import { healthCheckController } from './controllers/health-check/health-check'
-import rateLimit from 'express-rate-limit'
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 100,
-  standardHeaders: true,
-  legacyHeaders: false
-})
-
-export function router () {
+export function router() {
   const router = express.Router()
 
-  router.post('/shorturl', limiter, shortUrlController)
+  router.post('/shorturl', shortUrlController)
   router.get('/shorturl/analytics', shortUrlAnalyticsController)
   router.get('/shorturl/:shortId', redirectUrlController)
   router.get('/health', healthCheckController)
